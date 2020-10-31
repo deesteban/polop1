52f531d6b0a3c2d772655f82ea08f4455c1926dc
juanpolo <9002667@alumnos.ufv.es>
2020-10-31 13:40:54 +0100

    a

--- main.c
+++ main.c
@@ -1,11 +1,26 @@
+// Polo, Salinas, Juan
 #include <stdio.h>
-char **global_argv;
-int global_argc;
-int a;
+// Resto de include necesarios
 
-int main(int argc, char **argv) {
-    global_argv = argv;
-    global_argc = argc;
-    printf("hola\n");
-    return 0;
+void ejercicio1(){
+    // TO-DO
+}
+
+void ejercicio2(){
+    // TO-DO
+}
+
+void ejercicio3(){
+    // TO-DO
+}
+
+int main(){
+    // TO-DO Menú principal
+
+    // Para invocar a los ejercicios. Solo a modo de ejemplo y uso
+
+    ejercicio1(); // Salta al codigo de ejercicio 1 y cuando termina vuelve a este punto
+    ejercicio2(); // Salta al codigo de ejercicio 1 y cuando termina vuelve a este punto
+    ejercicio3(); // Salta al codigo de ejercicio 1 y cuando termina vuelve a este punto
+
 }
