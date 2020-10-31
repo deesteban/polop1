9e42452824c539c6fec4a5213f1d597c2faecd11
Jaime de Esteban <jaimedeesteban@protonmail.com>
2020-10-31 14:15:58 +0100

    a

--- main.c
+++ main.c
@@ -2,22 +2,31 @@
 #include <stdio.h>
 // Resto de include necesarios
 
-void ejercicio1(){
+void ejercicio1() {
     // TO-DO
 }
 
-void ejercicio2(){
+void ejercicio2() {
     // TO-DO
 }
 
-void ejercicio3(){
+void ejercicio3() {
     // TO-DO
 }
 
-int main(){
-    // TO-DO Menú principal
+int main() {
 
-    // Para invocar a los ejercicios. Solo a modo de ejemplo y uso
+    int resultado = 0;
+
+    printf("1.- Ejercicio 1\n2.- Ejercicio 2\n3.- Ejercicio 3\n4.- Salir\n\nIntroduzca una opcion(1-4) ");
+
+    scanf("%d", &resultado);
+
+    printf(resultado);
+
+    if (resultado > 4 || resultado < 1) {
+        printf("ERROR: Opcion introducida no valida");
+    }
 
     ejercicio1(); // Salta al codigo de ejercicio 1 y cuando termina vuelve a este punto
     ejercicio2(); // Salta al codigo de ejercicio 1 y cuando termina vuelve a este punto
