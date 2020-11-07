58e6cec141f377088d24f5fb51e3e7ed46959b95
Jaime de Esteban <jaimedeesteban@protonmail.com>
2020-11-07 21:57:40 +0100

    copia mid

--- main.c
+++ main.c
@@ -7,9 +7,9 @@
 void ejercicio1() {
 
     int radio = 10;
-    float porcentaje = 10;
-    float res;
-    float res2;
+    float porcentaje = 10.0f;
+    float res = 0.0f;
+    float res2 = 0.0f;
 
     for (int i = 2 * radio; i >= 0; i--) {
         for (int j = 0; j <= 1.5 * radio; j++) {
@@ -44,11 +44,11 @@ void ejercicio2() {
     int numerosRango = 0;
     int longitud = 0;
     int primerNumero = 0;
-    int factor;
-    int resultado;
-    int digito;
-    int resultadoTotal;
-    int itr;
+    int factor = 0;
+    int resultado = 0;
+    int digito = 0;
+    int resultadoTotal = 0;
+    int itr = 0;
 
     printf("Topper, introduce el codigo de activación (4 digitos)?\n");
     scanf("%d", &numerosTopper);
@@ -57,7 +57,7 @@ void ejercicio2() {
     primerNumero = numerosTopper / 1000;
 
     if (longitud == 4 && primerNumero != 0) {
-    //solo se entra si los numeros de topper son correctos
+        //solo se entra si los numeros de topper son correctos
 
         //comprobamos numeros de harriet
         printf("Harriet, introduce el codigo de activación (4 digitos)?\n");
@@ -87,18 +87,18 @@ void ejercicio2() {
                     //Solo entramos si todos los numeros estan correctos
 
                     resultadoTotal = 0;
-                    itr=1;
+                    itr = 1;
 
-                    for (int i = 9; i>= 2; i--){
-                        if (i>=5) {
+                    for (int i = 9; i >= 2; i--) {
+                        if (i >= 6) {
                             digito = (numerosHarriet / itr) % 10;
                             // el modulo 10 me da la cifra decimal del digito
                             factor = pow(2, i);
                             factor = factor % 11;
                             resultado = digito * factor;
                             resultadoTotal = resultadoTotal + resultado;
-                        } else if(i<=4){
-                            itr=1;
+                        } else if (i <= 5) {
+                            if (i == 5) itr = 1;
                             digito = (numerosTopper / itr) % 10;
                             // el modulo 10 me da el la cifra decimal del digito
                             factor = pow(2, i);
@@ -106,7 +106,7 @@ void ejercicio2() {
                             resultado = digito * factor;
                             resultadoTotal = resultadoTotal + resultado;
                         }
-                        itr=itr*10;
+                        itr = itr * 10;
                     }
 
                     resultadoTotal = resultadoTotal % 11;
@@ -128,25 +128,25 @@ void ejercicio2() {
                     //segundo digito
 
                     resultadoTotal = 0;
-                    itr=1;
+                    itr = 1;
 
                     // hacemos lo mismo que hemos hecho arriba, cambiando la posicion de los numeros
                     for (int i = 9; i >= 0; i--) {
-                        if (i>=5) {
+                        if (i >= 5) {
                             digito = (numerosRango / itr) % 10;
                             factor = pow(2, i);
                             factor = factor % 11;
                             resultado = digito * factor;
                             resultadoTotal = resultadoTotal + resultado;
-                        } else if(i<=4){
-                            itr=1;
+                        } else if (i <= 4) {
+                            if (i == 4) itr = 1;
                             digito = (numerosSpewart / itr) % 10;
                             factor = pow(2, i);
                             factor = factor % 11;
                             resultado = digito * factor;
                             resultadoTotal = resultadoTotal + resultado;
                         }
-                        itr=itr*10;
+                        itr = itr * 10;
                     }
 
                     resultadoTotal = resultadoTotal % 11;
@@ -186,6 +186,129 @@ void ejercicio2() {
 
 void ejercicio3() {
 
+    int pasillos = 0, filas = 0, huecos = 0, opcion = 0, pasilloDestino = 0, filaDestino = 0, huecoDestino = 0;
+    int robot[] = {0, 0, 0};
+    int todoOk = 0;
+    do {
+        printf("Numero de pasillos? ");
+        scanf("%d", &pasillos);
+        if (pasillos > 0) {
+            printf("\nFilas de cada pasillo? ");
+            scanf("%d", &filas);
+            if (filas > 0) {
+                printf("\nNumero de huecos de cada fila? ");
+                scanf("%d", &huecos);
+                if (huecos > 0) todoOk = 1;
+                else printf("El numero de pasillos debe de ser un numero positivo mayor que 0");
+            } else printf("El numero de pasillos debe de ser un numero positivo mayor que 0");
+        } else printf("El numero de pasillos debe de ser un numero positivo mayor que 0");
+
+    } while (todoOk != 1);
+
+    int contenedor[pasillos + 2][filas][huecos];
+
+    do {
+
+        printf("Operacion a realizar\n[1] Recoger\n[2] Almacenar\n[3] Salir\nOpcion? ");
+
+        scanf("%d", &opcion);
+
+        if (opcion == 2) {
+
+            todoOk = 0;
+
+            printf("\nDonde almacenar?");
+
+            do {
+                printf("Pasillo? ");
+                scanf("%d", &pasilloDestino);
+                if (pasilloDestino > 0 && pasilloDestino <= pasillos) {
+                    printf("\nFila?");
+                    scanf("%d", &filaDestino);
+                    filaDestino -= 1;
+                    if (filaDestino > 0 && filaDestino <= filas) {
+                        printf("\nHueco?");
+                        scanf("%d", &huecoDestino);
+                        huecoDestino -= 1;
+                        if (huecoDestino > 0 && huecoDestino <= huecos) todoOk = 1;
+                        else printf("Hueco no valido");
+                    } else printf("Fila no valida");
+                } else printf("Pasillo no valido");
+
+            } while (todoOk != 1);
+
+
+            if (contenedor[pasilloDestino][filaDestino][huecoDestino] != 1) {
+
+                printf("\nPosicion Robot - pasillo: %d, fila: %d, hueco: %d\n", robot[0], robot[1] + 1, robot[2] + 1);
+                printf("mov\t pasillo\t\t\tfila\t\t\thueco\n===============================================\n");
+                printf("-\t\t%d\t\t\t\t%d\t\t\t\t%d\t\n", robot[0], robot[1] + 1, robot[2] + 1);
+                if (robot[2] != 0) {
+                    for (int i = robot[2]; i > 0; i--) {
+                        robot[2]--;
+                        printf("L\t\t%d\t\t\t\t%d\t\t\t\t%d\t\n", robot[0], robot[1] + 1, robot[2] + 1);
+                    }
+                }
+
+                if (robot[1] != 0) {
+                    for (int i = robot[1]; i > 0; i--) {
+                        robot[1]--;
+                        printf("D\t\t%d\t\t\t\t%d\t\t\t\t%d\t\n", robot[0], robot[1] + 1, robot[2] + 1);
+                    }
+                }
+
+                // 7 0 0
+
+                // 4 0 1
+
+                //
+
+                if (robot[0] != 0) {
+                    for (int i = robot[0]; i > 0; i--) {
+                        robot[0]--;
+                        printf("B\t\t%d\t\t\t\t%d\t\t\t\t%d\t\n", robot[0], robot[1] + 1, robot[2] + 1);
+                    }
+                }
+
+                printf("Recogida de material\n");
+
+                if (robot[0] != pasilloDestino) {
+                    int maxIteraciones = abs(robot[0] - pasilloDestino);
+                    for (int i = 0; i < maxIteraciones; i++) {
+                        if (robot[0] >= pasilloDestino) {
+                            robot[0]--;
+                            printf("B\t\t%d\t\t\t\t%d\t\t\t\t%d\t\n", robot[0], robot[1] + 1, robot[2] + 1);
+                        } else {
+                            robot[0]++;
+                            printf("F\t\t%d\t\t\t\t%d\t\t\t\t%d\t\n", robot[0], robot[1] + 1, robot[2] + 1);
+                        }
+                    }
+                }
+
+                if (robot[1] != filaDestino) {
+                    for (int i = 0; i < filaDestino; i++) {
+                        robot[1]++;
+                        printf("U\t\t%d\t\t\t\t%d\t\t\t\t%d\t\n", robot[0], robot[1] + 1, robot[2] + 1);
+                    }
+                }
+
+                if (robot[2] != huecoDestino) {
+                    for (int i = 0; i < huecoDestino; i++) {
+                        robot[2]++;
+                        printf("R\t\t%d\t\t\t\t%d\t\t\t\t%d\t\n", robot[0], robot[1] + 1, robot[2] + 1);
+                    }
+                }
+
+                printf("Almacenado de material\n");
+                contenedor[pasilloDestino][filaDestino][huecoDestino] = 1;
+
+            } else {
+                printf("Posicion no valida, ya existe mercancia almacenada en la posicion indicada.\n");
+            }
+
+        }
+
+    } while (opcion != 3);
 
 
 }
